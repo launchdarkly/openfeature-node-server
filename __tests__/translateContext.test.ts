0f2b7a665a06a5da7bf0f0ee886e997d11da2526
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2023-02-15 10:34:54 -0800

    feat!: Implement context support. (#16)
    build(deps): Update the node-server-sdk to version 7.x.
    
    Co-authored-by: Matthew M. Keeler <keelerm84@gmail.com>

--- __tests__/translateContext.test.ts
+++ __tests__/translateContext.test.ts
@@ -3,12 +3,24 @@ import TestLogger from './TestLogger';
 
 it('Uses the targetingKey as the user key', () => {
   const logger = new TestLogger();
-  expect(translateContext(logger, { targetingKey: 'the-key' })).toEqual({ key: 'the-key' });
+  expect(translateContext(logger, { targetingKey: 'the-key' })).toEqual({ key: 'the-key', kind: 'user' });
   expect(logger.logs.length).toEqual(0);
 });
 
+it('gives targetingKey precedence over key', () => {
+  const logger = new TestLogger();
+  expect(translateContext(
+    logger,
+    { targetingKey: 'target-key', key: 'key-key' },
+  )).toEqual({
+    key: 'target-key',
+    kind: 'user',
+  });
+  // Should log a warning about both being defined.
+  expect(logger.logs.length).toEqual(1);
+});
+
 describe.each([
-  ['secondary', 'value1'],
   ['name', 'value2'],
   ['firstName', 'value3'],
   ['lastName', 'value4'],
@@ -26,20 +38,26 @@ describe.each([
     )).toEqual({
       key: 'the-key',
       [key]: value,
+      kind: 'user',
     });
     expect(logger.logs.length).toEqual(0);
   });
 });
 
+it.each(['key', 'targetingKey'])('handles key or targetingKey', (key) => {
+  const logger = new TestLogger();
+  expect(translateContext(
+    logger,
+    { [key]: 'the-key' },
+  )).toEqual({
+    key: 'the-key',
+    kind: 'user',
+  });
+  expect(logger.logs.length).toEqual(0);
+});
+
 describe.each([
-  ['secondary', false],
   ['name', 17],
-  ['firstName', new Date()],
-  ['lastName', true],
-  ['email', () => { }],
-  ['avatar', {}],
-  ['ip', []],
-  ['country', 42],
   ['anonymous', 'value'],
 ])('given incorrect built-in attributes', (key, value) => {
   it('the bad key is omitted', () => {
@@ -49,6 +67,7 @@ describe.each([
       { targetingKey: 'the-key', [key]: value },
     )).toEqual({
       key: 'the-key',
+      kind: 'user',
     });
     expect(logger.logs[0]).toMatch(new RegExp(`The attribute '${key}' must be of type.*`));
   });
@@ -58,19 +77,10 @@ it('accepts custom attributes', () => {
   const logger = new TestLogger();
   expect(translateContext(logger, { targetingKey: 'the-key', someAttr: 'someValue' })).toEqual({
     key: 'the-key',
-    custom: {
-      someAttr: 'someValue',
-    },
+    kind: 'user',
+    someAttr: 'someValue',
   });
   expect(logger.logs.length).toEqual(0);
-});
-
-it('ignores custom attributes that are objects', () => {
-  const logger = new TestLogger();
-  expect(translateContext(logger, { targetingKey: 'the-key', someAttr: {} })).toEqual({
-    key: 'the-key',
-  });
-  expect(logger.logs[0]).toEqual("The attribute 'someAttr' is of an unsupported type 'object'");
 });
 
 it('accepts string/boolean/number arrays', () => {
@@ -82,27 +92,12 @@ it('accepts string/boolean/number arrays', () => {
     booleans: [true, false],
   })).toEqual({
     key: 'the-key',
-    custom: {
-      strings: ['a', 'b', 'c'],
-      numbers: [1, 2, 3],
-      booleans: [true, false],
-    },
+    kind: 'user',
+    strings: ['a', 'b', 'c'],
+    numbers: [1, 2, 3],
+    booleans: [true, false],
   });
   expect(logger.logs.length).toEqual(0);
-});
-
-it('discards invalid array types', () => {
-  const logger = new TestLogger();
-  expect(translateContext(
-    logger,
-    {
-      targetingKey: 'the-key',
-      dates: [new Date()],
-    },
-  )).toEqual({
-    key: 'the-key',
-  });
-  expect(logger.logs[0]).toEqual("The attribute 'dates' is an unsupported array type.");
 });
 
 it('converts date to ISO strings', () => {
@@ -113,9 +108,121 @@ it('converts date to ISO strings', () => {
     { targetingKey: 'the-key', date },
   )).toEqual({
     key: 'the-key',
-    custom: {
-      date: date.toISOString(),
-    },
+    kind: 'user',
+    date: date.toISOString(),
   });
+  expect(logger.logs.length).toEqual(0);
+});
+
+it('can convert a single kind context', () => {
+  const evaluationContext = {
+    kind: 'organization',
+    targetingKey: 'my-org-key',
+  };
+
+  const expectedContext = {
+    kind: 'organization',
+    key: 'my-org-key',
+  };
+
+  const logger = new TestLogger();
+  expect(translateContext(logger, evaluationContext)).toEqual(expectedContext);
+  expect(logger.logs.length).toEqual(0);
+});
+
+it('can convert a multi-context', () => {
+  const evaluationContext = {
+    kind: 'multi',
+    organization: {
+      targetingKey: 'my-org-key',
+      myCustomAttribute: 'myAttributeValue',
+    },
+    user: {
+      targetingKey: 'my-user-key',
+    },
+  };
+
+  const expectedContext = {
+    kind: 'multi',
+    organization: {
+      key: 'my-org-key',
+      myCustomAttribute: 'myAttributeValue',
+    },
+    user: {
+      key: 'my-user-key',
+    },
+  };
+
+  const logger = new TestLogger();
+  expect(translateContext(logger, evaluationContext)).toEqual(expectedContext);
+  expect(logger.logs.length).toEqual(0);
+});
+
+it('can handle privateAttributes in a single context', () => {
+  const evaluationContext = {
+    kind: 'organization',
+    name: 'the-org-name',
+    targetingKey: 'my-org-key',
+    myCustomAttribute: 'myCustomValue',
+    privateAttributes: ['myCustomAttribute'],
+  };
+
+  const expectedContext = {
+    kind: 'organization',
+    name: 'the-org-name',
+    key: 'my-org-key',
+    myCustomAttribute: 'myCustomValue',
+    _meta: {
+      privateAttributes: ['myCustomAttribute'],
+    },
+  };
+
+  const logger = new TestLogger();
+  expect(translateContext(logger, evaluationContext)).toEqual(expectedContext);
+  expect(logger.logs.length).toEqual(0);
+});
+
+it('detects a cycle and logs an error', () => {
+  const a = {
+    b: { c: {} },
+  };
+
+  a.b.c = a;
+  const evaluationContext = {
+    key: 'a-key',
+    kind: 'singularity',
+    a,
+  };
+
+  const expectedContext = {
+    key: 'a-key',
+    kind: 'singularity',
+    a: { b: {} },
+  };
+
+  const logger = new TestLogger();
+  expect(translateContext(logger, evaluationContext)).toEqual(expectedContext);
+  expect(logger.logs.length).toEqual(1);
+});
+
+it('allows references in different branches', () => {
+  const a = { test: 'test' };
+
+  const evaluationContext = {
+    key: 'a-key',
+    kind: 'singularity',
+    b: { a },
+    c: { a },
+  };
+
+  const expectedContext = {
+    key: 'a-key',
+    kind: 'singularity',
+    b: { a: { test: 'test' } },
+    c: { a: { test: 'test' } },
+  };
+
+  const logger = new TestLogger();
+  expect(translateContext(logger, evaluationContext)).toEqual(expectedContext);
   expect(logger.logs.length).toEqual(0);
 });
